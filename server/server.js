91a139226897d73054bccc5d91292b61546f56ea
saurav <sauravsinghrauthan@gmail.com>
2021-02-15 10:24:24 +0530

    GET /todos

--- server/server.js
+++ server/server.js
@@ -22,9 +22,18 @@ app.post('/todos', (req, res) => {
     })
 });
 
-// app.get("/todos", (req, res) => {
-  
-// });
+app.get("/todos", (req, res) => {
+    Todo.find()
+    .then((todos) => {
+        res.status(200).
+        send({
+            todos
+        })
+    },
+    (err) => {
+        res.status(400).send(err)
+    })
+});
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
 
